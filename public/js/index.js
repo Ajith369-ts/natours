bb1c7fcfa3728272137e2231425e187b2e7d4889
ajith <ajithkumar11121@gmail.com>
2023-09-28 00:14:05 +0530

    disable stripe

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ import "@babel/polyfill";
 // import { displayMap } from "./mapbox";
 import { login, logout } from "./login";
 import { updateSettings } from "./updateSettings";
-import { bookTour } from "./stripe";
+// import { bookTour } from "./stripe";
 import { showAlert } from "./alerts";
 
 // const mapBox = document.getElementById("map");
@@ -65,7 +65,7 @@ if (bookBtn)
     bookBtn.addEventListener("click", (e) => {
         e.target.textContent = "Processing...";
         const { tourId } = e.target.dataset;
-        bookTour(tourId);
+        // bookTour(tourId);
     });
 
 const alertMessage = document.querySelector("body").dataset.alert;
--- utils/email.js
+++ utils/email.js
@@ -7,7 +7,7 @@ module.exports = class Email {
         this.to = user.email;
         this.firstName = user.name.split(" ")[0];
         this.url = url;
-        this.from = `Ajith Kumar <${process.ennv.EMAIL_FROM}>`;
+        this.from = `Ajith Kumar <${process.env.EMAIL_FROM}>`;
     }
 
     newTransport() {
